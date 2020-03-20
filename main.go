9beae8f9a8df32030c194ebbd6e3b21ad44e29b7
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-03-20 11:07:08 +0100

    guessing game

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Guess the number")
+
+	// generate a random number
+	source := rand.NewSource(time.Now().UnixNano())
+	randomizer := rand.New((source))
+	secretNumber := randomizer.Intn(100) // generates number between 0 and 100
+
+	var guess int
+	guessnum := 5
+
+	for guessnum > 0 {
+		fmt.Println("Please input your guess")
+		fmt.Scan(&guess)
+		if guess > secretNumber {
+			fmt.Println("Too Big")
+		} else if guess < secretNumber {
+			fmt.Println("Too small")
+		} else {
+			fmt.Println("You Win!")
+			break
+		}
+		guessnum = guessnum - 1
+	}
+
+	if guessnum == 0 {
+		fmt.Println("You lose!")
+	}
+
+}
